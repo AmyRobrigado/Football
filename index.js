ed2529ad15127aad741d081a1056d468cf822e20
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-11-02 21:58:58 -0400

    Modified files

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 "use strict";
 
-window.onload = "init";
-
-let teams = [
+window.onload = function()
+{
+  let teams = [
     {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
     {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
     {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
     {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},
-   
-    ];
+    ]; 
 
-    function dropMenuBtn()
+    const teamList = document.getElementById("teamList");
+
+    let length = teams.length;
+    let team = [];
+    for (let i = 0; i < length; i++)
     {
-       let dropMenuBtn = document.getElementById("dropMenuBtn");
-       document.getElementById("teamInfo").value = dropMenuBtn.li[dropMenuBtn.selectedIndex].text;
+      let optSelect = document.createElement("option");
+
+      if(teams[i].code==.name)
+      {
+         team.push(teams[i].name);
+      }
     }
+    return team;
+}
+
+   
+
+
+
+
+
+
