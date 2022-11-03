d46989f5714724b3a792cbc313b34058c7d88896
AmyRobrigado <amyantanod@gmail.com>
2022-11-03 10:37:31 -0400

    Modified file

--- index.js
+++ index.js
@@ -1,31 +1,86 @@
 "use strict";
 
-window.onload = function()
-{
-  let teams = [
-    {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
-    {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
-    {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
-    {code:"KAN", name:"Kansas City Chiefs", plays:"Kansas City, MO"},
-    ]; 
+window.onload = init;
 
-    const teamList = document.getElementById("teamList");
 
-    let length = teams.length;
-    let team = [];
-    for (let i = 0; i < length; i++)
-    {
-      let optSelect = document.createElement("option");
+const teams = [
+  { code: "DAL", name: "Dallas Cowboys", plays: "Arlington, TX" },
+  { code: "DEN", name: "Denver Broncos", plays: "Denver, CO" },
+  { code: "HOU", name: "Houston Texans", plays: "Houston, TX" },
+  { code: "KAN", name: "Kansas City Chiefs", plays: "Kansas City, MO" },
+];
 
-      if(teams[i].code==.name)
-      {
-         team.push(teams[i].name);
-      }
-    }
-    return team;
+
+function init() {
+  console.log("index.js");
+  fillDropdown();
+  const selectBtn = document.getElementById("selectBtn");
+  selectBtn.onclick = selectBtnOnClick;
+  //document.getElementById("selectBtn") = selectBtnOnClick;
+
 }
 
-   
+function fillDropdown() { // To create the dropdown menu
+  const teamSelect = document.getElementById("teamSelect");
+  let selectTeamOption = document.createElement("option");
+  selectTeamOption.value = "";
+  selectTeamOption.textContent = "Select a Team...";
+  teamSelect.appendChild(selectTeamOption);
+
+  let teamsLength = teams.length;
+  for (let i = 0; i < teamsLength; i++) {
+    let newOption = document.createElement("option");
+    newOption.value = teams[i].code;
+    newOption.textContent = teams[i].name;
+
+    teamSelect.appendChild(newOption);
+  }
+
+}
+
+function selectBtnOnClick() {
+  const teamSelect = document.getElementById("teamSelect");
+  let selectedTeamCode = teamSelect.value;
+  let selectedTeam = getTeamFromCode(teams, selectedTeamCode);
+
+  let message = `You selected the ${selectedTeam.name} (${selectedTeam.code}) who play in ${selectedTeam.plays}.`
+  const teamInfo = document.getElementById("teamInfo");
+  teamInfo.innerHTML = message;
+  return false;
+}
+
+function getTeamFromCode(teams, code) { 
+  let teamscount = teams.length;
+  for (let i = 0; i < teamscount; i++) {
+    if (teams[i].code == code) {
+      return teams[i];
+    }
+  }
+  return null;
+}
+
+// function selectBtnOnClick()
+// {
+//   const teamSelect = document.getElementById("teamSelect");
+//   teamSelect.code = "DAL";
+
+//   let length = teamSelect.options.length;
+//   for (let i = 0;i < length; i++)
+//   {
+//     if (teamSelect.options[i].name == .code)
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
